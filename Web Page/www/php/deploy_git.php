c7c20e3efd3ad1b08663a12979701149684893bf
Travis <bangstk@knights.ucf.edu>
2015-07-07 13:23:32 -0400

    Add git deploy script

---
+++ Web Page/www/php/deploy_git.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+This script is called from a Github WebHook whenever there is a push to the repo.
+
+This causes the server to pull from the repo and update the website.
+**/
+
+$REPO_PULL_LOC = "/var/www/gitpull2";
+$WEB_FOLDER_NAME = "Web\ Page/www";
+$COPY_LOC = "/var/www/html";
+$REPO = "https://github.com/sammyr2011/Task_Master.git";
+
+if ( isset($_POST['payload']) )
+{
+	//Pull from repo
+	shell_exec("cd {$REPO_PULL_LOC} && git pull");
+	
+	//Copy web folder contents to actual web folder on server
+	shell_exec("cd {$REPO_PULL_LOC}/{$WEB_FOLDER_NAME} && rsync -r --delete . {$COPY_LOC}");
+
+	exit("Received payload and updated server");
+}
+else
+	exit("Rejected; no payload");
+
+?>
