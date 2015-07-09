9248c842c4e89d80cbfed7a132960c6f0539e2a7
Travis <bangstk@knights.ucf.edu>
2015-07-09 12:55:01 -0400

    I'm dumb. Called a function like a variable...

--- Web Page/www/php/task_list.php
+++ Web Page/www/php/task_list.php
@@ -5,29 +5,27 @@ require_once 'db_connect.php';
 
 function listByCategory($incatid)
 {
-	
-	$tasksarray = array();
-	echo 'test1';
-	$dbhandle = $db_connect();
-	echo 'test2';
+	$tasks = array();
+
+	$dbhandle = db_connect();
+
 	$query = "SELECT TaskID FROM Tasks";
 	
 	if ($incatid != 0)
 		$query = "SELECT TaskID FROM Tasks WHERE Category={$incatid}";
 	
-	echo 'test3';
 	$result = $dbhandle->query($query);
-	echo 'test4';
+
 	while ($row = $result->fetch_array())
 	{
 		$newtask = new task();
 		$newtask->getFromDB($row['TaskID']);
-		array_push($tasksarray, $newtask);
+		array_push($tasks, $newtask);
 	}
 	
 	$dbhandle->close();
 	
-	return $tasksarray;
+	return $tasks;
 }
 
 ?>
