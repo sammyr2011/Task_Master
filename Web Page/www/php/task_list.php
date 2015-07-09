fed496dd1dea359b17941db69e6e082018b98c59
Travis <bangstk@knights.ucf.edu>
2015-07-09 12:39:19 -0400

    Fix if statement typo

--- Web Page/www/php/task_list.php
+++ Web Page/www/php/task_list.php
@@ -9,7 +9,7 @@ function listByCategory($incatid)
 	
 	$dbhandle = $db_connect();
 	
-	if ($incatid = NULL)
+	if ($incatid == NULL)
 		$query = "SELECT TaskID FROM Tasks";
 	else
 		$query = "SELECT TaskID FROM Tasks WHERE Category={$incatid}";
