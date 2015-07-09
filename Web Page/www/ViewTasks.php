065a88261516c7652dde3693d93a3ff136144483
Travis <bangstk@knights.ucf.edu>
2015-07-09 13:02:11 -0400

    Fix layout weirdness

--- Web Page/www/ViewTasks.php
+++ Web Page/www/ViewTasks.php
@@ -142,7 +142,8 @@ $tasks = listByCategory($catid);
 		
 		?>
         
-            <a href="ViewTask.php?id=<?php echo $task->taskid; ?>" class="row" style="border:solid lightgrey 2px;border-radius: 3px 3px 3px 3px;padding: 14px 26px 26px;">
+            <a href="ViewTask.php?id=<?php echo $task->taskid; ?>">
+			<div class="row" style="border:solid lightgrey 2px;border-radius: 3px 3px 3px 3px;padding: 14px 26px 26px;">
                 <div class="col-md-4 col-sm-4 col-xs-4">
                     <img src="images/oil.jpg" height="100px">
                 </div>
@@ -151,6 +152,7 @@ $tasks = listByCategory($catid);
                     <p><?php echo $task->description; ?></p>
                     <p>Current bid: <b>US $45.00</b></p>
                 </div>
+			</div>
             </a>
         
 		
