edeca42de99bb61f10feb0bd10b870a417ed2889
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-13 21:48:14 -0400

    Update category_list_android.php

--- Web Page/www/php/category_list_android.php
+++ Web Page/www/php/category_list_android.php
@@ -1,1 +1,17 @@
 <!-- JSON encodes current list of categories -->
+<?php
+  require_once 'db_connect.php';
+  $dbhandle = db_connect();
+  $query = "SELECT * FROM Categories";
+  $result $dbhandle->query($query);
+  $categories = array();
+  
+	while ($row = $result->fetch_array())
+	{
+		array_push($categories, $row);
+	}
+	
+	$dbhandle->close();
+	
+	json_encode $categories;
+?>
