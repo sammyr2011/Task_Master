c575d8d653a2fbbb1a6ed52265b4c29b96e25936
colbytroutt <colby.troutt@knights.ucf.edu>
2015-07-14 12:22:56 -0400

    Made categories match DB.

--- Web Page/www/ViewTasks.php
+++ Web Page/www/ViewTasks.php
@@ -122,12 +122,12 @@ $tasks = listByCategory($catid);
             <div class="well bs-sidebar" id="sidebar" style="height:80%;width:150px;">
                 <ul class="nav nav-pills nav-stacked">
                     <li><p style="text-align:center"><b>Categories:</b></p></li>
-                    <li><a href="ViewTasks.php?Categoryid=1">Academics</a></li>
-                    <li><a href="ViewTasks.php?Categoryid=2">Automotive</a></li>
-                    <li><a href="ViewTasks.php?Categoryid=3">Food</a></li>
-                    <li><a href="ViewTasks.php?Categoryid=4">Lawn</a></li>
+                    <li><a href="ViewTasks.php?Categoryid=1">Automotive</a></li>
+                    <li><a href="ViewTasks.php?Categoryid=2">Food</a></li>
+                    <li><a href="ViewTasks.php?Categoryid=3">Lawn Care</a></li>
+                    <li><a href="ViewTasks.php?Categoryid=4">Academics</a></li>
                     <li><a href="ViewTasks.php?Categoryid=5">House Work</a></li>
-                    <li><a href="ViewTasks.php?Categoryid=6">Other/Misc.</a></li>
+                    <li><a href="ViewTasks.php?Categoryid=6">Other</a></li>
                 </ul>
             </div>
         </div>
