d1fb458c5d27b8efa8f307a31fafbfcf71351b47
colbytroutt <colby.troutt@knights.ucf.edu>
2015-07-14 13:43:34 -0400

    Taking user out of ActiveUsers DB.
    Also added a couple of comments.

--- Web Page/www/Logout.php
+++ Web Page/www/Logout.php
@@ -1,8 +1,22 @@
 <?php 
+
+require_once 'db_connect.php'
+
 if (session_status() == PHP_SESSION_NONE) 
 {
 	session_start();
 }
+
+// Taking the user out of the ActiveUser DB.
+$dbhandle = db_connect();
+
+$sqlquery = "DELETE FROM ActiveUsers WHERE UserID = {$_SESSION["userid"]}";
+$result = $dbhandle->query($sqlquery);
+
+dbclose($dbhandle);
+
+
+// Closing the session.
 session_unset();
 session_destroy();
 
