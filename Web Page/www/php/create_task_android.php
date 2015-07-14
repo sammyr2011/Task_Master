f6eede46c86c50b08a41627d7827f2eb3c6514c0
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-14 09:17:30 -0400

    Update create_task_android.php

--- Web Page/www/php/create_task_android.php
+++ Web Page/www/php/create_task_android.php
@@ -9,6 +9,12 @@ $task->createFromPost($_POST);
 //Try to put task into DB
 $errors = $task->register();
 
+//If no errors, return a happy message
+if($errors==NULL)
+{
+  $errors['submitted']=true;
+}
+
 //Echo errors as JSON object
 echo json_encode($errors);
 
