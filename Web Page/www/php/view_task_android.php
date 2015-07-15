133bbb05daaeeedeefedc12488e0ed422bb821c1
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-15 13:47:02 -0400

    Update view_task_android.php

--- Web Page/www/php/view_task_android.php
+++ Web Page/www/php/view_task_android.php
@@ -1,1 +1,16 @@
+<?php
 
+require_once 'task_class.php';
+
+$intaskid = $_POST['TaskID'];
+
+$task = new task();
+
+$error = $task->getFromDB($intaskid);
+
+if($error != NULL)
+{
+  echo $task;
+}
+
+?>
