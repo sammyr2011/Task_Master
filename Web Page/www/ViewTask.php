f9ee67d52953cd19a4f01e46b4ffa9b1eb9029bd
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-20 16:49:46 -0400

    Update ViewTask.php

--- Web Page/www/ViewTask.php
+++ Web Page/www/ViewTask.php
@@ -182,10 +182,10 @@ else
                     <h1 style="font-size:18px;font-weight: bold;line-height: normal;margin: 0px;padding:0px;"><?php echo $task->title; ?></h1>
 					
 					<p style="font-size:small;color:#777 !important;margin:0px;padding0px;font-weight:normal;line-height:normal;"><?php echo $task->description; ?></p>
-					
+					<br>
                     <p>Current bid: <b>US $45.00</b></p>
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <p>Time left to Bid:</p>
+
+                    <p>Time left to Bid:</p><br>
                     <span id="countdown"></span>
 
                     <script>
@@ -222,8 +222,7 @@ else
                         }, 1000);
 
                     </script>
-
-                    </div>
+                    <br>
 
                     <!-- style="background-color: #E2E2E2;" -->
                     <div class="col-md-3 col-sm-3 col-xs-3" >
