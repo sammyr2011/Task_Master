4ac37e0d309c2a9fc976af7a1357f7921f1918d5
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-23 16:51:00 -0400

    Updated Messaging.php
    Example format v1

--- Web Page/www/Messaging.php
+++ Web Page/www/Messaging.php
@@ -1,9 +1,137 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Samuel Roman
- * Date: 7/21/2015
- * Time: 2:06 PM
- */
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-?>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <meta charset="utf-8">
+    <title>Task Master</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- CSS -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+
+        /* Sticky footer styles
+        -------------------------------------------------- */
+
+        html,
+        body {
+            height: 100%;
+            /* The html and body elements cannot have any padding or margin. */
+        }
+
+        /* Wrapper for page content to push down footer */
+        #wrap {
+            min-height: 100%;
+            height: auto !important;
+            height: 100%;
+            /* Negative indent footer by it's height */
+            margin: 0 auto -60px;
+        }
+
+        /* Set the fixed height of the footer here */
+        #push,
+        #footer {
+            height: 60px;
+        }
+        #footer {
+            background-color: #222;
+        }
+
+        /* Lastly, apply responsive CSS fixes as necessary */
+        /*@media (max-width: 767px) {
+             #footer {
+                 margin-left: -20px;
+                 margin-right: -20px;
+                 padding-left: 20px;
+                 padding-right: 20px;
+             }
+         }*/
+
+        @import url('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css');
+
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+</head>
+
+<body>
+
+
+<!-- Part 1: Wrap all page content here -->
+<div id="wrap">
+
+    <!-- Fixed navbar -->
+    <?php include "NavMod.php"; ?>
+
+    <!-- Begin page content -->
+    <div class="container-fluid">
+        <div class="col-md-4 col-sm-4 col-xs-4" style="border: solid 3px black">
+            <legend>Inbox</legend>
+
+            <div class="col-md-12 col-sm-12 col-xs-12" data-spy="scroll" style="height:100%;width:30%">
+
+
+
+                <!--
+                    When a new message is sent to user create new div in the format
+                    found below.
+
+                    Will hold the message box data
+
+                -->
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <!-- Stores user avatar -->
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+
+                    </div>
+                    <!-- Holds Username of sender and Task associated -->
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+
+                    </div>
+                    <!-- Date and time recieved -->
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+                        <p>7/23/2015 3:30 PM</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 col-sm-8 col-xs-8">
+
+        </div>
+    </div>
+    <div id="push"></div>
+</div>
+
+<div id="push"></div>
+
+<div id="footer">
+    <div class="container">
+        <p class="muted credit" style="color: white">COP 4331 Project 2</p>
+    </div>
+</div>
+
+
+</body>
+</html>
