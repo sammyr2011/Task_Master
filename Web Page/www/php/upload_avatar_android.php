f13b38dcd4c27067cf0d283c2191de0be4ef4947
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-24 13:06:41 -0400

    Update upload_avatar_android.php

--- Web Page/www/php/upload_avatar_android.php
+++ Web Page/www/php/upload_avatar_android.php
@@ -1,4 +1,19 @@
 <?
 require_once 'user_class.php';
-
+if(isset($_POST['UserID']) && isset($_POST['Img']))
+{
+  $userid = $_POST['UserID'];
+  $image = $_POST['Img'];
+  
+  $user = new user();
+  $user->getFromDB($userid);
+  
+  echo json_encode($user->uploadAvatar($image));
+}
+else
+{
+  $error['error']=true;
+  $error['UserID or Img not set']=true;
+  echo json_encode()
+}
 ?>
