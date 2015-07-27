7102f52405d8765fa8ee004af348e1024f671180
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-27 19:08:45 -0400

    Updated CreateTask.php

--- Web Page/www/CreateTask.php
+++ Web Page/www/CreateTask.php
@@ -264,13 +264,13 @@ if (isset($_POST['submit']))
 
 
                         <!-- Select End Date-->
-                        <b>End Bidding Date:</b> <input type="text" name="biddate" id="datepicker" placeholder="Click Here" class="form-control">
+                        <b>End Bidding Date:</b> <input type="text" name="biddate" id="datepicker" placeholder="Click Here" class="form-control input-group">
 
                         <!-- Time to end bidding -->
                         <div class="bfh-timepicker" data-mode="12h" placeholder="Select Time"></div>
 
                         <!-- When to have the job done by-->
-                        <b>Task Completion Date:</b> <input type="text" name="jobdate" id="datepicker2" placeholder="Click Here" class="form-control">
+                        <b>Task Completion Date:</b> <input type="text" name="jobdate" id="datepicker2" placeholder="Click Here" class="form-control input-group">
 
                         <!-- Time to task should be completed -->
                         <div class="bfh-timepicker" data-mode="12h" placeholder="Select Time"></div>
