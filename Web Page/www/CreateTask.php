61f8d060ea162e94679c4449106b4cd1b2fd7262
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-27 18:47:16 -0400

    Updated CreateTask.php

--- Web Page/www/CreateTask.php
+++ Web Page/www/CreateTask.php
@@ -268,7 +268,7 @@ if (isset($_POST['submit']))
 
 
                         <!-- Time to end bidding -->
-                        <div class="col-md-12 col-sm-12 col-xs-12 bfh-timepicker form-control" data-mode="12h"></div>
+                        <div class="col-md-12 col-sm-12 col-xs-12 bfh-timepicker" data-mode="12h"></div>
 
                         <script>
                             $().bfhtimepicker("toggle");
@@ -276,6 +276,9 @@ if (isset($_POST['submit']))
 
                         <!-- When to have the job done by-->
                         <p><b>Task Completion Date:</b> <input type="text" name="jobdate" id="datepicker2" placeholder="Click Here" class="form-control"></p>
+
+                        <!-- Time to task should be completed -->
+                        <div class="col-md-12 col-sm-12 col-xs-12 bfh-timepicker" data-mode="12h"></div>
 
                          <!-- File Button -->
                         <div class="control-group">
