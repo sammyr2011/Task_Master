59a8d3112db3db83894a0641330b0393e3e29e5b
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-27 20:17:29 -0400

    Updated ViewTask.php

--- Web Page/www/ViewTask.php
+++ Web Page/www/ViewTask.php
@@ -217,7 +217,7 @@ if (isset($_POST['submit']))
                         // find the amount of "seconds" between now and target
                         var current_date = new Date().getTime();
                         //var seconds_left = (target_date - current_date) / 1000;
-                        var seconds_left = 50;
+                        var seconds_left = current_date;
 
 
                         // do some time calculations
