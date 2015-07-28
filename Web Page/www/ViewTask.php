42a1a11983cf9e5f8ff89ce6db64fbfe9c4aef39
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-27 20:16:19 -0400

    Updated ViewTask.php

--- Web Page/www/ViewTask.php
+++ Web Page/www/ViewTask.php
@@ -235,7 +235,7 @@ if (isset($_POST['submit']))
                             + minutes + "m, " + seconds + "s";
 
                         //should terminate when countdown is done
-                        if(seconds<=0 && minutes<=0 && days<=0 && hours<=0)
+                       /* if(seconds<=0 && minutes<=0 && days<=0 && hours<=0)
                         {
                             seconds=0;
                             minutes=0;
@@ -244,7 +244,7 @@ if (isset($_POST['submit']))
                             document.getElementById("bidtime").innerHTML="Final Bid: ";
                             clearInterval(timerID);
                             return;
-                        }
+                        }*/
 
                     }, 1000);
 
