1d333d40f98f9d56a9dcf1aedbefb9bc88f4e6f9
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-30 16:31:22 -0400

    Update leave_feedback_android.php

--- Web Page/www/php/leave_feedback_android.php
+++ Web Page/www/php/leave_feedback_android.php
@@ -1,5 +1,14 @@
 <?php
 require_once 'user_class.php';
 
+$review = new review();
+$error = $review->initialize($_POST);
+
+if($error == null)
+{
+  $error['Success']=true;
+}
+
+echo json_encode($error);
 
 ?>
