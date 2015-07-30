6097042ec0e2dca5e2d004dafa0973a3e9fbe155
Travis <bangstk@knights.ucf.edu>
2015-07-30 13:51:08 -0400

    Supply DoerOrLister arg

--- Web Page/www/php/lister.php
+++ Web Page/www/php/lister.php
@@ -90,7 +90,7 @@ function listReviewsByTime($inuser)
 	while ($row = $result->fetch_array())
 	{
 		$newreview = new review();
-		$newreview->getFromDB($row['RatingID']);
+		$newreview->getFromDB($row['RatingID'], 0);
 		array_push($reviews, $newreview);
 	}
 	
@@ -112,7 +112,7 @@ function listDoReviewsByTime($inuser)
 	while ($row = $result->fetch_array())
 	{
 		$newreview = new review();
-		$newreview->getFromDB($row['RatingID']);
+		$newreview->getFromDB($row['RatingID'], 1);
 		array_push($reviews, $newreview);
 	}
 	
