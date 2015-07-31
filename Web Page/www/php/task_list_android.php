8dba0f9ee84257961cf14bede1a7dba5a4adea36
AndrewVance <andrewvance@knights.ucf.edu>
2015-07-30 21:12:49 -0400

    Update task_list_android.php

--- Web Page/www/php/task_list_android.php
+++ Web Page/www/php/task_list_android.php
@@ -1,16 +1,17 @@
 <?php
-
-require_once 'task_list.php';
+require_once 'lister.php';
 
 if(isset($_POST['catid']))
 {
-  $incatid = $_POST['catid'];
+  echo json_encode(listTasksByCategory($_POST['catid']));
+}
+else if(isset($_POST['userid']))
+{
+  echo json_encode(listTasksByUser($_POST['userid']));
 }
 else
 {
-  $incatid = 0;
+  echo json_encode(listTasksByCategory(0));
 }
-
-echo json_encode(listByCategory($incatid));
 
 ?>
