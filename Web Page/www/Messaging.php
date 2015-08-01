71d9ff2c3379e5b4a310f31121595a1c8b62432c
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-07-31 22:13:23 -0400

    Updated Messaging.php

--- Web Page/www/Messaging.php
+++ Web Page/www/Messaging.php
@@ -160,7 +160,7 @@
             </div>
             <div class="col-md-8 col-sm-8 col-xs-8" >
                 <div class="box" style="border-style:solid;border-color:lightgrey;height:750px">
-                    <h1 class="text-center" style="color:red;border-bottom: solid lightgrey;margin-bottom: 20px;font-size: large">Username(other user)</h1>
+                    <h1 class="text-center" style="color:red;border-bottom: solid lightgrey;margin-bottom: 20px;height: 30px;">Username(other user)</h1>
 
                     <!-- Storing messages in striped rows for contrast
 
