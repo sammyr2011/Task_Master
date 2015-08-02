5713f386eac9c53aa8eabd75cde9b66bab748538
Tarvis451 <bangstk@knights.ucf.edu>
2015-08-02 16:12:47 -0400

    It's UPDATE not MODIFY for MySQL

--- Web Page/www/php/message_class.php
+++ Web Page/www/php/message_class.php
@@ -100,7 +100,7 @@ class message
 		$dbhandle = db_connect();
 		
 		$stmt = $dbhandle->stmt_init();
-		$stmt->prepare("MODIFY Messages SET Read=1 WHERE MessageID=?");
+		$stmt->prepare("UPDATE Messages SET Read=1 WHERE MessageID=?");
 		$stmt->bind_param("i", $this->messageID);
 		$stmt->execute();
 		
