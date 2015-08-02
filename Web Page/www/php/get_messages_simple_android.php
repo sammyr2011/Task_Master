e770506a58a0c73b39db53e941539f9ab87b6bc5
AndrewVance <andrewvance@knights.ucf.edu>
2015-08-02 19:34:56 -0400

    Create get_messages_simple_android.php

---
+++ Web Page/www/php/get_messages_simple_android.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'message_class.php';
+require_once 'user_class.php';
+require_once 'db_connect.php';
+
+$out = array();
+
+if(isset($_SESSION['userid']))
+{
+	$messages = array();
+	
+	$dbhandle = db_connect();
+	$stmt = $dbhandle->stmt_isnit();
+
+	$stmt->prepare("SELECT MessageID, Time, SenderID, ReadFlag FROM Messages WHERE (ReceiverID=?)");
+	$stmt->bind_param("ii",$inUserID,$_SESSION['userid']);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($messagedid, $temptime, $senderid, $readflag);	
+
+	while($stmt->fetch())
+	{
+		$message = new message();
+		$message->getFromDB($messageid);
+		$sender = new user();
+		$sender->getFromDB($message['SenderID']);
+		$message['SenderUsername']=$sender->username;
+
+		array_push($out,$message);
+	}
+
+	$stmt->close();
+	$dbhandle->close();
+}
+else
+{
+	$out['error']=true;
+}
+
+echo json_encode($out);
+
+
+?>
