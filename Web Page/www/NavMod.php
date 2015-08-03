312946e25f87bd54e566f7ecf3d88480e01a3d6c
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-08-02 21:27:45 -0400

    Updated NavMod.php

--- Web Page/www/NavMod.php
+++ Web Page/www/NavMod.php
@@ -14,8 +14,8 @@ require_once 'php/message_lister.php';
 
 
 
-$convoUsers = array();
-$convoUsers = getUnreadMessages($_SESSION['userid']);
+$unread = array();
+$unread = getUnreadMessages($_SESSION['userid']);
 
 ?>
 
@@ -36,7 +36,7 @@ $convoUsers = getUnreadMessages($_SESSION['userid']);
                     <li><a href="CreateTask.php">Create Task</a></li>
                     <li><a href="ViewTasks.php">View Tasks</a></li>
                     <!-- Maybe add in parenthesis how many unread messages the user has like "Messge Center (5)" -->
-                    <li><a href="MessageCenter.php">Message Center<span id="NewMessages" style="color:red"> (<?php echo count($convoUsers) ?>)</span></a></li>
+                    <li><a href="MessageCenter.php">Message Center<span id="NewMessages" style="color:red"> (<?php echo count($unread) ?>)</span></a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
 					<?php if (!isset($_SESSION['username']))
