a3dd83aba05cf745c1250067be49e1c73151ba67
sammyr2011 <sammyr2011@knights.ucf.edu>
2015-08-03 23:02:37 -0400

    Updated index.php

--- Web Page/www/index.php
+++ Web Page/www/index.php
@@ -90,7 +90,7 @@
 
 	<?php include "php/alerts.php"; ?>
         <section class="block" style="background-color:#222;">
-            <div id="myCarousel" class="carousel slide" style="margin-bottom: 50px;margin-top:-40px;padding-top:40px;" >
+            <div id="myCarousel" class="carousel slide" style="margin-bottom: 50px;margin-top:-40px;padding-top:40px;padding-bottom:40px;" >
                 <div class="carousel-inner text-center">
                     <div class="active item">
                         <img src="images/baking.jpg" alt="Slide1" style="margin: auto auto auto 230px"/>
