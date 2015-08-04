fc42f11cae2eb379fe0a5e033b8e198d9e8e4791
sammyr2011 <sammyr2011@knights.ucf.edu>
2015-08-03 21:12:24 -0400

    Updated Index.php

--- Web Page/www/index.php
+++ Web Page/www/index.php
@@ -89,46 +89,28 @@
     <div class="container">
 
 	<?php include "php/alerts.php"; ?>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="images/mowinggrass.jpg" alt="tutoring" style="width:auto;height:460px;margin: 0 auto">
-                    <div class="carousel-caption">
+        <section class="block">
+            <div id="myCarousel" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="active item">
+                        <img src="images/baking.jpg" alt="Slide1" />
+                    </div>
+                    <div class="item">
+                        <img src="images/lawn2.jpg" alt="Slide2" />
+                    </div>
+                    <div class="item">
+                        <img src="images/car%20cleaning.jpg" alt="Slide3" />
                     </div>
                 </div>
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">›</a>
 
-                <div class="item">
-                    <img src="images/baking.jpg" alt="baking" style="width:auto;height:460px;margin: 0 auto">
-                    <div class="carousel-caption">
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src="images/oil.jpg" alt="CarWork" style="width:auto;height:460px;margin: 0 auto">
-                    <div class="carousel-caption">
-                    </div>
-                </div>
             </div>
+        </section>
 
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-		<div id="push"></div>
+
+
+        <div id="push"></div>
 		<div class="row">
 			<div class="col-md-6 col-sm-6 col-xs-6 text-right">
 				<a href="CreateTask.php"><button type="button" class="btn btn-primary btn-lg raised">Create Task</button></a>
