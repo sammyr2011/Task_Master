d81597872e5cf348eb919103a29ad8397d68da9d
sammyr2011 <sammyr2011@knights.ucf.edu>
2015-08-19 16:20:36 -0400

    Testing Google Maps API

--- Web Page/www/ViewTasks.php
+++ Web Page/www/ViewTasks.php
@@ -185,6 +185,33 @@ function countDownFunc(enddate, divid)
 
 </script>
 
+<!-- Google Maps API -->
+<script
+    src="http://maps.googleapis.com/maps/api/js">
+</script>
+<script>
+    var myCenter=new google.maps.LatLng(51.508742,-0.120850);
+
+    function initialize()
+    {
+        var mapProp = {
+            center:myCenter,
+            zoom:5,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+
+        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var marker=new google.maps.Marker({
+            position:myCenter,
+        });
+
+        marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
 <body>
 
 
@@ -196,6 +223,7 @@ function countDownFunc(enddate, divid)
     
     <!-- Begin page content -->
     <div class="container-fluid">
+        <div class="row" id="googleMap" style="width:100%;height:380px;"></div>
         <div class="col-md-2 col-sm-2 col-xs-2">
             <div class="well bs-sidebar" id="sidebar" style="height:80%;width:150px;">
                 <ul class="nav nav-pills nav-stacked">
